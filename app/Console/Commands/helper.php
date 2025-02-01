7c1a004c34f9a2572997d2b39dcee44816fa0553
Brian Christian Hanjaya <brian.hanjaya@kly.id>
2025-02-01 17:53:39 +0800

    gas

--- app/Console/Commands/helper.php
+++ app/Console/Commands/helper.php
@@ -29,82 +29,83 @@ class helper extends Command
      */
     public function handle()
     {
-        $gramatiks = Gramatik::get();
-        $redemittels = Redemittel::get();
-        $vocabs = Vocab::get();
-        foreach ($gramatiks as $gramatik) {
-            $gramatik->update([
-                'kapital' => $gramatik->kapital . '-'
-            ]);
-        }
-        foreach ($redemittels as $redemittel) {
-            $redemittel->update([
-                'kapital' => $redemittel->kapital . '-'
-            ]);
-        }
-        foreach ($vocabs as $vocab) {
-            $vocab->update([
-                'kapital' => $vocab->kapital . '-'
-            ]);
-        }
+        //add - di kapital
+        // $gramatiks = Gramatik::get();
+        // $redemittels = Redemittel::get();
+        // $vocabs = Vocab::get();
+        // foreach ($gramatiks as $gramatik) {
+        //     $gramatik->update([
+        //         'kapital' => $gramatik->kapital . '-'
+        //     ]);
+        // }
+        // foreach ($redemittels as $redemittel) {
+        //     $redemittel->update([
+        //         'kapital' => $redemittel->kapital . '-'
+        //     ]);
+        // }
+        // foreach ($vocabs as $vocab) {
+        //     $vocab->update([
+        //         'kapital' => $vocab->kapital . '-'
+        //     ]);
+        // }
 
 
 
         //ordering
-        // $gramatiks = Gramatik::get();
-        // $order = [];
-        // foreach ($gramatiks as $gramatik) {
-        //     $kapital = $gramatik->kapital;
-        //     $parts = explode('-', $kapital);
-        //     $lastPart = end($parts);
-        //     $startValue = reset($parts);
+        $gramatiks = Gramatik::get();
+        $order = [];
+        foreach ($gramatiks as $gramatik) {
+            $kapital = $gramatik->kapital;
+            $parts = explode('-', $kapital);
+            $lastPart = end($parts);
+            $startValue = reset($parts);
 
-        //     if (!in_array($startValue, $order)) {
-        //         $order[$startValue] = [];
-        //     }
+            if (!in_array($startValue, $order)) {
+                $order[$startValue] = [];
+            }
 
-        //     if (!in_array($lastPart, $order[$startValue])) {
-        //         $order[$startValue][$lastPart] = 1;
-        //     } else {
-        //         $order[$startValue][$lastPart]++;
-        //     }
+            if (!in_array($lastPart, $order[$startValue])) {
+                $order[$startValue][$lastPart] = 1;
+            } else {
+                $order[$startValue][$lastPart]++;
+            }
 
-        //     switch ($startValue) {
-        //         case 'A1':
-        //             $add1 = 1000;
-        //             break;
-        //         case 'A2':
-        //             $add1 = 2000;
-        //             break;
-        //         case 'B1':
-        //             $add1 = 3000;
-        //             break;
-        //         case 'B2':
-        //             $add1 = 4000;
-        //             break;
-        //         case 'C1':
-        //             $add1 = 5000;
-        //             break;
-        //         case 'C2':
-        //             $add1 = 6000;
-        //             break;
-        //     }
-        //     $add1 = $add1 + ((int)$lastPart * 10);
+            switch ($startValue) {
+                case 'A1':
+                    $add1 = 1000;
+                    break;
+                case 'A2':
+                    $add1 = 2000;
+                    break;
+                case 'B1':
+                    $add1 = 3000;
+                    break;
+                case 'B2':
+                    $add1 = 4000;
+                    break;
+                case 'C1':
+                    $add1 = 5000;
+                    break;
+                case 'C2':
+                    $add1 = 6000;
+                    break;
+            }
+            $add1 = $add1 + ((int)$lastPart * 10);
 
-        //     $order[] = $kapital;
-        //     $gramatik->update([
-        //         'order' =>  $add1 + $order[$startValue][$lastPart]
-        //     ]);
-        // }
-        // $gramatiks = Gramatik::orderBy('order')->get();
+            $order[] = $kapital;
+            $gramatik->update([
+                'order' =>  $add1 + $order[$startValue][$lastPart]
+            ]);
+        }
+        $gramatiks = Gramatik::orderBy('order')->get();
 
-        // $c = 0;
-        // foreach ($gramatiks as $gramatik) {
-        //     $c++;
-        //     $gramatik->update([
-        //         'order' =>  $c
-        //     ]);
-        // }
-        //
+        $c = 0;
+        foreach ($gramatiks as $gramatik) {
+            $c++;
+            $gramatik->update([
+                'order' =>  $c
+            ]);
+        }
+        
     }
 }
