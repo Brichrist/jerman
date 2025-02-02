4bb074a260e0f5b2c44ef139aadeb9404a3c9b73
Brian Christian Hanjaya <brian.hanjaya@kly.id>
2025-02-02 20:33:19 +0800

    gas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,30 @@
+import Alpine from "alpinejs";
 
-import Alpine from 'alpinejs';
-
-
-import './bootstrap';
+import "./bootstrap";
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
-import jQuery from 'jquery';
+import jQuery from "jquery";
 window.$ = jQuery;
 
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 window.Swal = Swal;
+
+$(document).ready(function () {
+    $(".spc-btn").on("click", function (e) {
+        e.preventDefault();
+        var text = $(this).text().trim();
+        console.log(text);
+        copyToClipboard(text);
+    });
+});
+
+function copyToClipboard(text) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
